a3beb0b1249d0d8f8b85a3231d922313f8555721
Hafida-mh <79710349+Hafida-mh@users.noreply.github.com>
2021-11-14 11:25:15 +0100

    hafida modif navForm

--- client/src/Component/navFomule/index.js
+++ client/src/Component/navFomule/index.js
@@ -2,37 +2,58 @@ import React from 'react';
 import  './navformule.css';
 import { FaArrowCircleLeft,FaShoppingBag,FaFacebook, FaUber, FaPersonBooth, FaPhone, FaUser, FaBuilding} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import NavBar from '../navBar/NavBar'
+
 const NavFomule=()=>{
 
 return(<div className="wrap-nav">
+
+<NavBar />
+
 <div className="wrap-inter">
-<div className="nav-form-item ">
-<FaBuilding className="icon-nav-formule"/></div>
-<div className="nav-form-item">
+ {
+        /* <div className="nav-form-item ">
+   
+   
+<FaBuilding className="icon-nav-formule"/> 
+
+
+</div>*/}
+
+<div className="nav-form-item homeReturnButton">
     <div className="nav-inter-item">  
-    <div className="circle">1</div>Ajoutez vos options</div> 
+
+    <i class="fas fa-arrow-left"></i><div> Retour accueil </div>
+    </div> 
  </div>
 <div className="nav-form-item">
 <div className="nav-inter-item"> 
-    <div className="circle">2</div>Retourtnez à la page d'accueil</div>
+    <div className="circle">1</div>Formule Soto</div>
     </div>
 
 <div className="nav-form-item">
 <div className="nav-inter-item">
-    <div className="circle">3</div>Réservez votre déménagement</div>
+    <div className="circle">2</div>Formule chrono</div>
 </div>
-<div className="nav-form-item speration"></div>
-<div className="nav-form-item">
-<span className="text-phone">Besoin d'assistance ?</span>
+
+
+ {/* <div className="nav-form-item speration"></div> */}
+
+
+  {/*<div className="nav-form-item">
+   <span className="text-phone">Besoin d'assistance ?</span>
 <span><FaPhone className="icon-nav-formule"/>+33123366699 </span>
 
-</div>
-<div className="nav-form-item">
-    
+
+</div>*/}
+<div className="navRightIconSide"> 
+ <div className="nav-form-item">
     <FaUser className="icon-nav-formule"/>
     </div>
-<div className="nav-form-item ">
+
+ <div className="nav-form-item ">
     <FaShoppingBag className="icon-nav-formule"/>
+    </div>
     </div>
 </div>
     </div>)
